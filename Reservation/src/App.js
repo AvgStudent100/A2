b9ad8d2fd3bc6617a1b2ab66a989cead35706bcc
VR1306 <vijay13.doodleblue@gmail.com>
2022-03-20 19:50:26 +0530

    'Res'

---
+++ Reservation/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import Reser from './Reser'
+
+export class App extends Component {
+  render() {
+    return (
+      <div>
+       <Reser/>
+      </div>
+    )
+  }
+}
+
+export default App
